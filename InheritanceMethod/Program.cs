0344ced348b20ea4347b223683fa419d033cbc51
Jennifer Akosua Sunkwa <jasunkwa@st.ug.edu.gh>
2025-08-05 21:25:01 +0000

    task one completed

---
+++ InheritanceMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
+
